7b41138ab37e8d30ef8fc0fb40fb53b07e87f7b2
DAM <DAM@203-04>
2022-11-14 15:48:49 +0100

    He creat un menú.

--- src/conversio/Conversio.java
+++ src/conversio/Conversio.java
@@ -9,18 +9,56 @@ import java.util.Scanner;
 
 /**
  *
- * @author nil
+ * @author DAM
  */
 public class Conversio {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Introdueix en €");
-        int valor_euros = sc.nextInt();
+        int menu;
+        
+        do{
+            System.out.println("MENU");
+            System.out.println("1. Conversió d'EURO a LLIURA");
+            System.out.println("2. Conversió de LLIURA a EURO");
+            System.out.println("3. Sortir");
+            
+            System.out.println("\n Tria una opció: ");
+            
+            menu = sc.nextInt();
+            
+            
+            switch (menu){
+                case 1:
+                        System.out.println("\nCONVERSIÓ D'EUROS A LLIURES");
+                        System.out.println("Introdueix en €:");
+                        int valor_euros = sc.nextInt();
+
+                        double tipus_de_canvi = 1.15d;
+                        System.out.println(valor_euros + " euros (€) actualment equivalen a " + (valor_euros / tipus_de_canvi) + " lliures esterlines (£).");
+
+                        System.out.println("\nPrem INTRO per tornar al menú");
+                        sc.nextLine();
+                        sc.nextLine();
+                        
+                    break;
+                    
+                case 2:
+                    System.out.println("");
+                    
+             
+                    break;
+                    
+                case 3:
+                    System.out.println("\nHas escollit sortir. Adéu!");
+                    break;
+                    
+                default:
+                    System.out.println("\nOpció INCORRECTA, ha de ser de l'1 al 3");
+                }
                 
-        double tipus_de_canvi = 1.15d;
-        System.out.println(valor_euros + " euros (€) actualment equivalen a " + (valor_euros / tipus_de_canvi) + " lliures esterlines (£).");
+        } while (menu != 3);
+        
     }
-    
 }
